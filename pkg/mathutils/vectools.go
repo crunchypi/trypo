836bbf0965f3018ab3ab48311a521f95f7191d22
crunchypi <crunchypi@protonmail.com>
2021-07-19 14:51:27 +0200

    Added: VecDivScalar, VecMulScalar, VecAdd, VecSub

--- pkg/mathutils/vectools.go
+++ pkg/mathutils/vectools.go
@@ -30,3 +30,41 @@ func VecMean(generator func() ([]float64, bool)) ([]float64, bool) {
 
 	return res, true
 }
+
+func VecDivScalar(vec []float64, scalar float64) []float64 {
+	res := make([]float64, len(vec))
+	for i, v := range vec {
+		res[i] = v / scalar
+	}
+	return res
+}
+
+func VecMulScalar(vec []float64, scalar float64) []float64 {
+	res := make([]float64, len(vec))
+	for i, v := range vec {
+		res[i] = v * scalar
+	}
+	return res
+}
+
+func VecAdd(a, b []float64) ([]float64, bool) {
+	if len(a) != len(b) {
+		return nil, false
+	}
+	res := make([]float64, len(a))
+	for i := 0; i < len(a); i++ {
+		res[i] = a[i] + b[i]
+	}
+	return res, true
+}
+
+func VecSub(a, b []float64) ([]float64, bool) {
+	if len(a) != len(b) {
+		return nil, false
+	}
+	res := make([]float64, len(a))
+	for i := 0; i < len(a); i++ {
+		res[i] = a[i] - b[i]
+	}
+	return res, true
+}
