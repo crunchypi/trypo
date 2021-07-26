3aa9db58b5230f819a72310d620570a011be4867
crunchypi <crunchypi@protonmail.com>
2021-07-26 18:03:07 +0200

    refactor

--- pkg/kmeans/rpc/server.go
+++ pkg/kmeans/rpc/server.go
@@ -1,14 +1,18 @@
 package rpc
 
-func (s *KMeansServer) Vec(namespace string, resp *[]float64) error {
-	lookupOK := s.Table.Access(namespace, func(cm *CentroidManager) {
-		*resp = cm.Vec()
-	})
-
+// Reduces some boilerplate by doing the '!lookupOK { ... } return nil' thing.
+func (s *KMeansServer) handleNamespaceErr(ns string, f func(*CentroidManager)) error {
+	lookupOK := s.Table.Access(ns, f)
 	if !lookupOK {
-		return NamespaceErr{namespace}
+		return NamespaceErr{ns}
 	}
 	return nil
+}
+
+func (s *KMeansServer) Vec(namespace string, resp *[]float64) error {
+	return s.handleNamespaceErr(namespace, func(cm *CentroidManager) {
+		*resp = cm.Vec()
+	})
 }
 
 type AddDataPointArgs struct {
@@ -40,74 +44,41 @@ type DrainArgs struct {
 	Ordered   bool
 }
 
-type drainF = func(*CentroidManager) []DataPoint
-
-func (s *KMeansServer) drain(ns string, resp *[]DataPoint, f drainF) error {
-	lookupOK := s.Table.Access(ns, func(cm *CentroidManager) {
-		*resp = f(cm)
-	})
-	if !lookupOK {
-		return NamespaceErr{ns}
-	}
-	return nil
-}
-
 func (s *KMeansServer) DrainUnordered(args DrainArgs, resp *[]DataPoint) error {
-	return s.drain(args.NameSpace, resp, func(cm *CentroidManager) []DataPoint {
-		return cm.DrainUnordered(args.N)
+	return s.handleNamespaceErr(args.NameSpace, func(cm *CentroidManager) {
+		*resp = cm.DrainUnordered(args.N)
 	})
 }
 
 func (s *KMeansServer) DrainOrdered(args DrainArgs, resp *[]DataPoint) error {
-	return s.drain(args.NameSpace, resp, func(cm *CentroidManager) []DataPoint {
-		return cm.DrainOrdered(args.N)
+	return s.handleNamespaceErr(args.NameSpace, func(cm *CentroidManager) {
+		*resp = cm.DrainOrdered(args.N)
 	})
 }
 
 func (s *KMeansServer) Expire(namespace string, _ *int) error {
-	lookupOK := s.Table.Access(namespace, func(cm *CentroidManager) {
+	return s.handleNamespaceErr(namespace, func(cm *CentroidManager) {
 		cm.Expire()
 	})
-	if !lookupOK {
-		return NamespaceErr{namespace}
-	}
-	return nil
 }
 
 func (s *KMeansServer) LenDP(namespace string, resp *int) error {
-	lookupOK := s.Table.Access(namespace, func(cm *CentroidManager) {
+	return s.handleNamespaceErr(namespace, func(cm *CentroidManager) {
 		*resp = cm.LenDP()
 	})
-	if !lookupOK {
-		return NamespaceErr{namespace}
-	}
-	return nil
 }
 
 func (s *KMeansServer) MemTrim(namespace string, _ *int) error {
-	lookupOK := s.Table.Access(namespace, func(cm *CentroidManager) {
+	return s.handleNamespaceErr(namespace, func(cm *CentroidManager) {
 		cm.MemTrim()
 	})
-	if !lookupOK {
-		return NamespaceErr{namespace}
-	}
-	return nil
 }
 
 func (s *KMeansServer) MoveVector(namespace string, resp *bool) error {
-	lookupOK := s.Table.Access(namespace, func(cm *CentroidManager) {
+	return s.handleNamespaceErr(namespace, func(cm *CentroidManager) {
 		*resp = cm.MoveVector()
 	})
-	if !lookupOK {
-		return NamespaceErr{namespace}
-	}
-	return nil
 }
-
-/*
-Reason for why DistributeDataPoints method is missing is written in client.go,
-right after MoveVector.
-*/
 
 type DistribDPIArgs struct {
 	NameSpace string
@@ -115,13 +86,9 @@ type DistribDPIArgs struct {
 }
 
 func (s *KMeansServer) DistributeDataPointsInternal(args DistribDPIArgs, _ *int) error {
-	lookupOK := s.Table.Access(args.NameSpace, func(cm *CentroidManager) {
+	return s.handleNamespaceErr(args.NameSpace, func(cm *CentroidManager) {
 		cm.DistributeDataPointsInternal(args.N)
 	})
-	if !lookupOK {
-		return NamespaceErr{args.NameSpace}
-	}
-	return nil
 }
 
 type KNNLookupArgs struct {
@@ -132,13 +99,9 @@ type KNNLookupArgs struct {
 }
 
 func (s *KMeansServer) KNNLookup(args KNNLookupArgs, resp *[]DataPoint) error {
-	lookupOK := s.Table.Access(args.NameSpace, func(cm *CentroidManager) {
+	return s.handleNamespaceErr(args.NameSpace, func(cm *CentroidManager) {
 		*resp = cm.KNNLookup(args.Vec, args.K, args.Drain)
 	})
-	if !lookupOK {
-		return NamespaceErr{args.NameSpace}
-	}
-	return nil
 }
 
 type NearestCentroidArgs struct {
@@ -149,13 +112,9 @@ type NearestCentroidArgs struct {
 }
 
 func (s *KMeansServer) NearestCentroids(args NearestCentroidArgs, r *[]*Centroid) error {
-	lookupOK := s.Table.Access(args.NameSpace, func(cm *CentroidManager) {
+	return s.handleNamespaceErr(args.NameSpace, func(cm *CentroidManager) {
 		*r, _ = cm.NearestCentroids(args.Vec, args.N, args.Drain)
 	})
-	if !lookupOK {
-		return NamespaceErr{args.NameSpace}
-	}
-	return nil
 }
 
 type NearestCentroidVecArgs struct {
@@ -164,16 +123,12 @@ type NearestCentroidVecArgs struct {
 }
 
 func (s *KMeansServer) NearestCentroidVec(args NearestCentroidVecArgs, r *[]float64) error {
-	lookupOK := s.Table.Access(args.NameSpace, func(cm *CentroidManager) {
+	return s.handleNamespaceErr(args.NameSpace, func(cm *CentroidManager) {
 		centroids, _ := cm.NearestCentroids(args.Vec, 1, false)
 		if len(centroids) != 0 {
 			*r = centroids[0].Vec()
 		}
 	})
-	if !lookupOK {
-		return NamespaceErr{args.NameSpace}
-	}
-	return nil
 }
 
 type SplitCentroidsArgs struct {
@@ -183,15 +138,11 @@ type SplitCentroidsArgs struct {
 }
 
 func (s *KMeansServer) SplitCentroids(args SplitCentroidsArgs, _ *int) error {
-	lookupOK := s.Table.Access(args.NameSpace, func(cm *CentroidManager) {
+	return s.handleNamespaceErr(args.NameSpace, func(cm *CentroidManager) {
 		cm.SplitCentroids(func(c *Centroid) bool {
 			return c.LenDP() > args.DPRangeMin && c.LenDP() < args.DPRangeMax
 		})
 	})
-	if !lookupOK {
-		return NamespaceErr{args.NameSpace}
-	}
-	return nil
 }
 
 type MergeCentroidsArgs struct {
@@ -201,13 +152,9 @@ type MergeCentroidsArgs struct {
 }
 
 func (s *KMeansServer) MergeCentroids(args SplitCentroidsArgs, _ *int) error {
-	lookupOK := s.Table.Access(args.NameSpace, func(cm *CentroidManager) {
+	return s.handleNamespaceErr(args.NameSpace, func(cm *CentroidManager) {
 		cm.MergeCentroids(func(c *Centroid) bool {
 			return c.LenDP() > args.DPRangeMin && c.LenDP() < args.DPRangeMax
 		})
 	})
-	if !lookupOK {
-		return NamespaceErr{args.NameSpace}
-	}
-	return nil
 }
