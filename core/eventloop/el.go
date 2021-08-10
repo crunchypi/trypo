2a3625ac033d4519e9c17edef39082f4c2674534
crunchypi <crunchypi@protonmail.com>
2021-08-10 17:38:25 +0200

    rm arbiter in event-loop step + a bunch of configs
    Some additional off-branch testing of the arbiter
    pkg has revealed some flakeyness, consensus isn't
    always a thing, apparently. So removed that until
    it's actually needed from this file (el.go).
    
    Also added more configuration for the event loop.

--- core/eventloop/el.go
+++ core/eventloop/el.go
@@ -3,11 +3,82 @@ package eventloop
 import (
 	"time"
 	"trypo/pkg/arbiter"
-
-	"github.com/crunchypi/go-narb/apsa/rpc"
 )
 
 type Addr = arbiter.Addr
+
+// Each task in the event loop will be skippable such that not everything has
+// to run in each loop iteration. This is useful when some tasks are recource
+// and/or time intensive (more than others) and should run infrequently.
+// Example:
+//	int=1 : run on each loop.
+//	int=2 : run every second loop.
+//	int=3 : run every third loop.
+//	etc...
+// Values over 1000 will never be ran.
+type EventLoopTaskSkipConfig struct {
+	// Expire triggers datapoint expiration in the whole network.
+	// Not necessary to always do this, as moving dataponts will
+	// often auto-expire them, though this isn't garanteed.
+	Expire int
+	// MemTrim triggers memory reduction in the whole network.
+	// In practice, this means that slices containing datapoints
+	// and centroids have their capacity reduced. Not necessary
+	// to always do this, since slices might be re-populated with
+	// new data, thout that is not garuanteed.
+	MemTrim int
+	// DistributeDataPointsFast triggers the hasty movement of
+	// datapoints within the network. It is a way of attracting
+	// dps to some best-fit node, but with an accuracy/speed
+	// tradeoff (also see DistributeDataPointsFast field). It
+	// moved dps on a node granularity. The actual amount to
+	// distribute is set in EventLoopConfig.
+	DistributeDataPointsFast int
+	// DistributeDataPointsAccurate triggers movement of datapoints
+	// within the network with highest possible accuracy, though at
+	// the cost of speed. It is an alternative to DistributeDataPointsFast
+	// and works on a Centroid granularity (contained by nodes), as opposed
+	// to just nodes. The actual amount to distribute is set in EventLoopConfig.
+	DistributeDataPointsAccurate int
+	// DistributeDataPointsInternal triggers movement of datapoints
+	// within each node in the network, as opposed to between nodes.
+	// It can be thought of as data integrity on a node-level. The
+	// actual amount to distribute is set in EventLoopConfig.
+	DistributeDataPointsInternal int
+	// SplitCentroids triggers procedures in the network that splits
+	// centroids if they are too big. The threshold values are specified
+	// in EventLoopConfig.
+	SplitCentroids int
+	// MergeCentroids triggers procedures in the network that merges
+	// centroids if they are too small. The threshold values are
+	// specified in EventLoopConfig.
+	MergeCentroids int
+	// LoadBalancing triggers load-balancing in the network.
+	LoadBalancing int
+}
+
+// Clams vals in EventLoopTaskSkipConfig (particularly useful for
+// ensuring no zeros (zero div err when using modulus))
+func (cfg *EventLoopTaskSkipConfig) clamp(min, max int) {
+	items := []*int{
+		&cfg.Expire,
+		&cfg.MemTrim,
+		&cfg.DistributeDataPointsFast,
+		&cfg.DistributeDataPointsAccurate,
+		&cfg.DistributeDataPointsInternal,
+		&cfg.SplitCentroids,
+		&cfg.MergeCentroids,
+		&cfg.LoadBalancing,
+	}
+	for _, v := range items {
+		if *v < min {
+			*v = min
+		}
+		if *v > max {
+			*v = max
+		}
+	}
+}
 
 type eventLoopInternal struct {
 	stopped bool
@@ -15,20 +86,59 @@ type eventLoopInternal struct {
 }
 
 type EventLoopConfig struct {
-	LocalAddr   Addr
+	LocalAddr Addr
+	// All addresses in the network, should include LocalAddr.
 	RemoteAddrs []Addr
 
+	// Timeout for each loop interation.
 	TimeoutLoop time.Duration
+	// Timeout for each task in the event loop.
 	TimeoutStep time.Duration
+
+	// Each task in the event loop will be skippable. See doc for
+	// EventLoopTaskSkipConfig for more details.
+	TaskSkip EventLoopTaskSkipConfig
+
+	// Specifies how many datapoints each node in the network should
+	// distribute in haste (for data integrity in the network). This
+	// distribution variant works on a _node_ granularity. See also
+	// the DistributeDataPointsAccurate variant.
+	DistributeDataPointsFastN int
+	// Specifies how many datapoints each node in the network should
+	// distribute with accuracy (for data integrity in the network).
+	// This distribution variant works on a _centroid_ (as opposed
+	// to node) granularity (Centroids are a detail level below nodes).
+	DistributeDataPointsAccurateN int
+	// Specifies how many datapoints each node in the network should
+	// distribute internally (node and not network data integrity).
+	DistributeDataPointsInternalN int
+
+	// Centroids within a node can grow to big and should be split
+	// eventually. This is the lower end of the range in which centroids
+	// will be split.
+	SplitCentroidsMin int
+	// Centroids within a node can grow to big and should be split
+	// eventually. This is the upper end of the range in which centroids
+	// will be split.
+	SplitCentroidsMax int
+
+	// Centroid within a node can be reduced over time for various
+	// reasons (such as when contained datapoints are expired), and
+	// should be merged eventually. This is the lower end of the range
+	// in which centroids will be merged.
+	MergeCentroidsMin int
+	// Centroid within a node can be reduced over time for various
+	// reasons (such as when contained datapoints are expired), and
+	// should be merged eventually. This is the upper end of the range
+	// in which centroids will be merged.
+	MergeCentroidsMax int
 
 	// Added by event loop.
 	internal eventLoopInternal
 }
 
-// This is intended to be a thing that is called for each event-loop task
-// that does some network orchestration, so there will be only one node
-// in the network that does the 'task',
-func eventLoopStep(cfg *EventLoopConfig, task func(*EventLoopConfig)) {
+// Intended to be used for each event loop task in the event loop.
+func elStep(cfg *EventLoopConfig, task func(*EventLoopConfig)) {
 	// For quickly aborting all steps in eventloop.
 	if cfg.internal.stopped {
 		return
@@ -40,30 +150,6 @@ func eventLoopStep(cfg *EventLoopConfig, task func(*EventLoopConfig)) {
 		return
 	}
 
-	// Only do task if local is arbiter.
-	client := arbiter.ArbiterClient(cfg.LocalAddr, nil)
-	arbiterData := client.Arbiter()
-
-	// Something wrong, try to set global arbiter.
-	if arbiterData.Status != rpc.StatusOK {
-		clients := arbiter.ArbiterClients(cfg.RemoteAddrs, nil, nil)
-		ok := clients.TryForceNewArbiter(100)
-		if !ok {
-			panic("couldn't get arbiter consensus in event loop step")
-		}
-		// Retry getting arbiter.
-		arbiterData = client.Arbiter()
-	}
-
-	// Local is simply not arbiter.
-	if !arbiterData.Addr.Comp(cfg.LocalAddr) {
-		return
-	}
-	// Check again in case the arbiter resolution takes a while.
-	if cfg.internal.stopped {
-		return
-	}
-
 	task(cfg)
 }
 
@@ -71,6 +157,8 @@ func EventLoop(cfg *EventLoopConfig) func() {
 	if cfg == nil {
 		panic("nil eventloop cfg")
 	}
+
+	cfg.TaskSkip.clamp(1, 1000)
 
 	go func() {
 		// Note: tasks are _not_ arbitrarily ordered.
